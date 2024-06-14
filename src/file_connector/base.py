1ccffe53618e2ff2495c62c280bd44d03d6b0013
Ilya Novikov <illianovikovnitzana@gmail.com>
2024-06-14 13:38:36 +0300

    update file_connector\base.py

--- src/file_connector/base.py
+++ src/file_connector/base.py
@@ -6,12 +6,15 @@ from src.dto import Vacancy
 class FileConnector(ABC):
     @abstractmethod
     def get_vacancies(self) -> list[Vacancy]:
+        """Абстрактный метод для получения вакансий без обязательных атрибутов"""
         pass
 
     @abstractmethod
     def add_vacancy(self, vacancy: Vacancy):
+        """Абстрактный метод для добавления вакансий без обязательных атрибутов"""
         pass
 
     @abstractmethod
     def delete_vacancy(self, vacancy: Vacancy):
+        """Абстрактный метод для удаления вакансий без обязательных атрибутов"""
         pass
