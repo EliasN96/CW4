0d601207318f92fd7f858b8e4d6660346670c87d
Ilya Novikov <illianovikovnitzana@gmail.com>
2024-06-14 13:39:04 +0300

    update test_dto.py

--- tests/test_dto.py
+++ tests/test_dto.py
@@ -1,10 +1,13 @@
+import pytest
+
 from src.dto import Salary
 
 
 class TestSalaryCompare:
+    """Класс с методами для автоматического тестирования зарплаты"""
     def test_salary_are_equals_none_with_same_currency(self):
         """
-        Тест с одинаковыми от и до - зарплатами, а также валютой
+        Тест с одинаковыми от и до - зарплатами(None), а также валютой
         """
         salary_1 = Salary(salary_from=None, salary_to=None, currency='RUR')
         salary_2 = Salary(salary_from=None, salary_to=None, currency='RUR')
@@ -44,8 +47,41 @@ class TestSalaryCompare:
 
     def test_same_to_salaries_from_salary_equal_none(self):
         """
-                Тест с разной максимальной зарплатой
-                """
-        lower_salary = Salary(salary_from=40000, salary_to=60000, currency='RUR')
-        higher_salary = Salary(salary_from=40000, salary_to=70000, currency='RUR')
+        Тест с разной максимальной зарплатой
+        """
+        lower_salary = Salary(salary_from=None, salary_to=60000, currency='RUR')
+        higher_salary = Salary(salary_from=None, salary_to=70000, currency='RUR')
         assert lower_salary.salary_to < higher_salary.salary_to
+
+    def test_equal_salary_from(self):
+        """
+        Тест с одинаковой минимальной зарплатой
+        """
+        salary_1 = Salary(salary_from=50000, salary_to=60000, currency='RUR')
+        salary_2 = Salary(salary_from=50000, salary_to=70000, currency='RUR')
+        assert salary_1.salary_from == salary_2.salary_from
+
+    def test_equal_salary_to(self):
+        """
+        Тест с одинаковой максимальной зарплатой
+        """
+        salary_1 = Salary(salary_from=40000, salary_to=70000, currency='RUR')
+        salary_2 = Salary(salary_from=50000, salary_to=70000, currency='RUR')
+        assert salary_1.salary_to == salary_2.salary_to
+
+    def test_salary_from_less_than_salary_to(self):
+        """
+        Тест максимальной зарплаты выше минимальной
+        """
+        salary = Salary(salary_from=50000, salary_to=70000, currency='RUR')
+        assert salary.salary_to > salary.salary_from
+
+    def test_salary_from_more_than_salary_to(self):
+        """
+        Тест минимальной зарплаты выше максимальной
+        """
+        with pytest.raises(ArithmeticError) as ar:
+            salary = Salary(salary_from=70000, salary_to=50000, currency='RUR')
+            assert salary.min_salary_more_than_max_salary() == ar
+
+
